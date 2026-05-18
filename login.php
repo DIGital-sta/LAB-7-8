d9778ff3877b2a56102978b95e384745d1e9df6e
DIGital-sta <egorbajush@gmail.com>
2026-05-18 08:33:32 +0300

    jfjf

--- login.php
+++ login.php
@@ -269,12 +269,12 @@ if (isLoggedIn()) {
                     <div class="auth-group">
                         <label for="regUsername">Логін *</label>
                         <input type="text" id="regUsername" name="username"
-                               placeholder="ivan123" required minlength="3" autocomplete="username">
+                               placeholder="" required minlength="3" autocomplete="username">
                     </div>
                     <div class="auth-group">
                         <label for="regDisplayName">Ім'я</label>
                         <input type="text" id="regDisplayName" name="display_name"
-                               placeholder="Іван Іваненко">
+                               placeholder="">
                     </div>
                 </div>
 
